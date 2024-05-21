a52042c00610207ae4f3ec2f39ea534688a6a1ff
takmanfa <takman@facilityapps.com>
2024-05-21 09:34:09 +0200

    wip

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,27 +2,11 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             //TargetBadge.HeightRequest = 100;
             //TargetBadge.WidthRequest = 100;
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            TargetBadge.Rotation += 15;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
