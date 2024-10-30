bc352fc0b6e300982b966c95578a103da2f0f061
takmanfa <takman@facilityapps.com>
2024-10-30 13:21:43 +0100

    add fontawesome icons; add DocumentExpanderTemplate; add treeview to MainPage;

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,16 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-        }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            FolderList.SelectionBackground = Brush.Transparent;
+            //FolderList.ItemTemplate = new DocumentItemTemplateSelector();
+            FolderList.ExpanderTemplate = App.Current.Resources["DocumentExpanderTemplate"] as DataTemplate;
+            Color kleurtje = Color.FromArgb("");
+            //FolderList.ItemTapped += FolderListOnItemTapped;
+            //FolderList.NodeExpanded += OnNodeExpanded;
         }
     }
-
 }
