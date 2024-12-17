ca878a0d826c7a363475eaf94f2f86f37af10d11
takmanfa <takman@facilityapps.com>
2024-12-17 10:56:11 +0100

    forgot to stage this for the previous commit

--- WindowsSfListViewEventBug/App.xaml.cs
+++ WindowsSfListViewEventBug/App.xaml.cs
@@ -5,6 +5,8 @@ public partial class App : Application
     public App()
     {
         InitializeComponent();
+        
+        Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzYzMDQ4OEAzMjM4MmUzMDJlMzBPUG1tVGFsbHhlVjBnS0ZSZ3lHcWFBQTdhc285aC91N3B6YXpNYmx0eCtJPQ==");
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
--- WindowsSfListViewEventBug/MainPage.xaml.cs
+++ WindowsSfListViewEventBug/MainPage.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.ObjectModel;
+﻿using Syncfusion.Maui.ListView;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace WindowsSfListViewEventBug;
 
 public partial class MainPage : ContentPage
 {
     int count = 0;
+    int longPressCount = 0;
     public ObservableCollection<Item> ItemList = new();
     public ObservableCollection<Item> Items
     {
@@ -15,12 +18,19 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+        ListViewElement.ItemLongPress += OnItemLongPress;
         Items.Add(new Item { ItemText = "Hello" });
         Items.Add(new Item { ItemText = "Check" });
         Items.Add(new Item { ItemText = "Test" });
         Items.Add(new Item { ItemText = "One Two Three" });
         Items.Add(new Item { ItemText = "Message" });
         Items.Add(new Item { ItemText = "ASDF" });
+    }
+
+    private void OnItemLongPress(object? sender, ItemLongPressEventArgs e)
+    {
+        longPressCount++;
+        CounterBtn.Text = $"Longpressed {longPressCount} time(s)";
     }
 
     private void OnCounterClicked(object sender, EventArgs e)
